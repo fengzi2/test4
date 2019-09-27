50f7016a82b458e5d73e96232063d132b0c3826b
pjf <pjf116@126.com>
2019-09-27 13:49:18 +0800

    dddd

---
+++ src/test/main.java
@@ -0,0 +1,18 @@
+package test;
+
+public class main {
+    public static void main(String[] args) {  
+        //测试，传入多个参数  
+        test("hello", "world", "123", "china", "csdn", "com");  
+    	//test(new String[] {"hello", "world", "123", "china", "csdn", "com"});
+    }  
+
+    public static void test(String... arguments) {  
+        for (int i = 0; i < arguments.length; i++) {  
+            System.out.println(arguments[i]);  
+        }  
+    }  
+
+
+
+}
